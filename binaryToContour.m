4b2f130ad908db07cac80f5420acf53d269e22d2
tingleshao <tingleshao@gmail.com>
2013-04-15 11:30:33 -0400

    binary to contour

--- binaryToContour.m
+++ binaryToContour.m
@@ -1,15 +1,17 @@
 function [ contour_im ] = binaryToContour( binary_image_file_name )
 % convert binary image to contour image
-binary_im = imread('binary_image_file_name');
+binary_im = imread(binary_image_file_name);
 contour_im = zeros(size(binary_im));
 row = size(binary_im,1);
 col = size(binary_im,2);
 for i = 2:row-1,
     for j = 2:col-1,
-        if binary_im(i-1,j) == binary_im(i+1,j) == binary_im(i,j-1) == binary_im(i,j+1)
+        if binary_im(i-1,j) > 0  && binary_im(i+1,j) >0 && binary_im(i,j-1) >0 && binary_im(i,j+1) > 0
+            continue
+        elseif  binary_im(i-1,j) == 0  && binary_im(i+1,j) == 0 && binary_im(i,j-1) ==0 && binary_im(i,j+1) == 0
             continue
         else
-            contour_im(i,j) = 1
+            contour_im(i,j) = 1;
         end
     end
 end
